fbdfe8fdb5c5a197c14c052fd3a20abe5e177bb8
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-11-18 10:31:50 -0300

    changing directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
